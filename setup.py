8ff43e14b4062fc9132fd3da164c4666d0d12762
Steven Van Rossem <steven.vanrossem@intec.ugent.be>
2017-06-13 10:58:51 +0200

    install latest version of PyJWT as dependency

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         # in jenkins, the last package in the list is installed first
         install_requires=['setuptools', 'pyaml', 'jsonschema', 'validators',
                           'requests>2.4.2', 'coloredlogs<=5.1.1', 'paramiko',
-                          'termcolor', 'tabulate', 'networkx', 'PyJWT',
+                          'termcolor', 'tabulate', 'networkx', 'PyJWT>=1.4.2',
                           'Flask', 'docker==2.0.2', 'scipy', 'numpy',
                           'watchdog', 'Flask-Cors', 'flask_cache', 'redis',
                           'pycrypto', 'matplotlib', 'prometheus_client'],
